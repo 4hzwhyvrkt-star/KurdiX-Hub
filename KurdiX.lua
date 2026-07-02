644798a5bec198d0474e67d96c703a380d5631ec
KurdiX Dev <4hzwhyvrkt@privaterelay.appleid.com>
2026-07-02 21:40:05 +0300

    Update KurdiX.lua

--- KurdiX.lua
+++ KurdiX.lua
@@ -1,1 +1,12 @@
+-- // KurdiX Hub Fixed Version // --
+local Players = game:GetService("Players")
+local LP = Players.LocalPlayer
+local PlayerGui = LP:WaitForChild("PlayerGui") -- گۆڕرا بۆ PlayerGui
 
+local ScreenGui = Instance.new("ScreenGui", PlayerGui)
+ScreenGui.Name = "KurdiX_GUI"
+
+-- لێرەدا کۆدی مینیوەکەت دابنێ...
+-- (هەموو کۆدەکەی پێشووت لێرە پەیست بکە، بەڵام ئەم جارە بگۆڕە بۆ PlayerGui)
+
+print("KurdiX Hub Loaded Successfully!")
